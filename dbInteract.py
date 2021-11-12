3425d085ac352abab868c0d6fec34929287632ef
AbbyKloss <klossabby@gmail.com>
2021-11-12 00:19:21 -0600

    good menu

--- dbInteract.py
+++ dbInteract.py
@@ -1,21 +1,36 @@
 import sqlite3
 import random
+import getpass
 # import os
 
 con = sqlite3.connect("testdb.db")
 cur = con.cursor()
 
-adminFlag = 0
-logged_in = 0
+class User:
+    def __init__(self, usr="", bool=False, id=0):
+        self.name = usr
+        self.Admin = bool
+        self.ID = id
+    
+    def setName(self, str):
+        self.name = str
+    
+    def setAdmin(self, bool):
+        self.Admin = bool
+    
+    def setID(self, num):
+        self.ID = num
+
+    
 
 # possibly prone to sql injection
 # tried to do injection, it didn't work. there's a chance idk how to do it
 # returns -1 on failure, 0 on success
 def create_account():
-    usern = input("INPUT USERNAME:  ")
-    passw = input("INPUT PASSWORD:  ")
+    usern = input("Input Username:  ")
+    passw = input("Input Password:  ")
     # pass2 = input("VERIFY PASSWORD: ")
-    if (passw != input("VERIFY PASSWORD: ")):
+    if (passw != input("Verify Password: ")):
         print("Passwords don't match")
         return -1
     cur.execute("select exists(select * from Users where Username=?)", (usern, ))
@@ -33,36 +48,61 @@ def create_account():
     return 0
 
 def help(code):
-    if (code == 0):
-        print("Options: \n\tLogin,\n\tCreate Account,\n\tExit,\n\tHelp")
+    if (code == 0): # 0 is not logged in
+        print("Options: \n\tLogin\n\tCreate Account\n\tExit\n\tHelp")
+    if (code == 1):
+        print("Options: \n\texit \n\tidk lmao")
 
 def login():
-    int = 0
+    # int = 0
     usern = input("INPUT USERNAME: ")
-    passw = input("INPUT PASSWORD: ")
+    passw = getpass.getpass("INPUT PASSWORD: ")
     cur.execute("select exists(select * from Users where Username=? AND Password=?)", (usern, passw))
     int = cur.fetchone()[0]
     if not (int):
         print("Wrong username or password")
-    return int
+        return int, None
+    for row in cur.execute("select Admin, UserID from Users where Username=?", (usern, )):
+        print("", end="") # do nothing command
+    user = User(usern, row[0], row[1])
+    return int, user
+
+def searchByAuthor():
+    author = input("INPUT AUTHOR NAME: ")
+    for row in cur.execute("SELECT Title from Book where Author LIKE ?", (f"%{author}%", )):
+        print(row[0])
+
+# equivalent to a C main()
+# starting the not definition code here
+
+adminFlag = 0
+logged_in = 0
+kill = 0
+user = 0
 
 while not(logged_in):
     option = input("Guest % ").lower()
     if (option == "login"):
-        logged_in = login()
+        logged_in, user = login()
     elif (option == "create account"):
         create_account()
     elif (option == "exit"):
-        exit()
+        if (input("Exit? (Y/n): ").lower() == "y"):
+            exit()
     elif (option == "help"):
         help(0)
+    else:
+        print(f"Unknown command: '{option}', try 'help'")
 
+while(1):
+    option = input(f"{user.name} % ").lower()
+    if (option == "help"):
+        help(1)
+    elif (option == "exit"):
+        if (input("Exit? (Y/n)").lower() == "y"):
+            exit()
+    else:
+        print(f"Unknown command: '{option}', try 'help'")
 
-strong = ""
-author = input("INPUT AUTHOR NAME: ")
-for row in cur.execute("SELECT Title from Book where Author LIKE ?", (f"%{author}%", )):
-    strong += row[0]
-
-print(strong)
 con.close()
-print("done")
+# print("done")
